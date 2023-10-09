1b7f18fbe29525f31ad50f99129f0391e57872ea
osh <oh12sung@gmail.com>
2023-10-09 10:24:21 +0900

    Operator with Variable 업로드

---
+++ dags/dags_bash_with_variable.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.bash import BashOperator
+from airflow.models import Variable
+
+with DAG(
+    dag_id="dags_bash_with_variable",
+    schedule="10 9 * * *",
+    start_date=pendulum.datetime(2023, 4, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    # 1. Python 라이브러리를 사용
+    var_value = Variable.get("sample_key")
+
+    bash_var_1 = BashOperator(
+    task_id="bash_var_1",
+    bash_command=f"echo variable:{var_value}"
+    )
+
+    # 2. Jinja Template를 이용하여 Operator 내부에서 가져오기
+    bash_var_2 = BashOperator(
+    task_id="bash_var_2",
+    bash_command="echo variable:{{var.value.sample_key}}"
+    )
