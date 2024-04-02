4ae7c8d7814713f111b09ffec1e5de3ffa87e429
osh <oh12sung@gmail.com>
2024-04-02 22:22:14 +0900

    custom operator 실습

---
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -0,0 +1,61 @@
+from airflow.models.baseoperator import BaseOperator
+from airflow.hooks.base import BaseHook
+import pandas as pd
+
+class SeoulApiToCsvOperator(BaseOperator):
+    template_fields = ('endpoint', 'path', 'file_name', 'base_dt')
+
+    def __init__(self, dataset_nm, path, file_name, base_dt=None, **kwargs):
+        super().__init__(**kwargs)
+        self.http_conn_id = 'openapi.seoul.go.kr'
+        self.path = path
+        self.file_name = file_name
+        self.endpoint = '{{var.value.apikey_openapi_seoul_go_kr}}/json/' + dataset_nm
+        self.base_dt = base_dt
+
+    def execute(self, context):
+        import os
+
+        connection = BaseHook.get_connection(self.http_conn_id)
+        self.base_url = f'http://{connection.host}:{connection.port}/{self.endpoint}' # host:8080
+        
+        total_row_df = pd.DataFrame()
+        start_row = 1
+        end_row = 1000
+
+        while True:
+            self.log.info(f'시작:{start_row}')
+            self.log.info(f'끝:{end_row}')
+            row_df = self._call_api(self.base_url, start_row, end_row)
+            total_row_df = pd.concat([total_row_df, row_df])
+            if len(row_df) < 1000:
+                break
+            else:
+                start_row = end_row + 1
+                end_row += 1000
+
+        if not os.path.exist(self.path):
+            os.system(f'mkdir -p {self.path}')
+        total_row_df.to_csv(self.path + '/' + self.file_name, encoding='utf-8', index=False)
+
+    def _call_api(self, base_url, start_row, end_row):
+        import requests
+        import json
+
+        headers = {
+            'Content_Type': 'application/json',
+            'charset': 'utf-8',
+            'Accept': '*/*'
+        }
+
+        request_url = f'{base_url}/{start_row}/{end_row}/'
+        if self.base_dt is not None:
+            request_url = f'{base_url}/{start_row}/{end_row}/{self.base_dt}'
+        response = requests.get(request_url, headers)   # dictionary 구조를 띄지만 현재는 string 형태
+        contents = json.loads(response.text)    # string 값을 dictionary 형태로 반환
+
+        key_nm = list(contents.key())[0]    # 'stationInfo'
+        row_data = contents.get(key_nm).get('row')  # 'stationInfo > row > 리스트값
+        row_df = pd.DataFrame(row_data)
+
+        return row_df
