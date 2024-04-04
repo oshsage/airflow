4f9d434970b30ef9b8cf7b05c94cf6c1728c82c0
osh <oh12sung@gmail.com>
2024-04-04 22:13:31 +0900

    [Custom Operator] 코드 수정

--- dags/dags_seoul_api_corona.py
+++ dags/dags_seoul_api_corona.py
@@ -5,7 +5,7 @@ import pendulum
 with DAG(
     dag_id='dags_seoul_api_corona',
     schedule='0 7 * * *',
-    start_date=pendulum.datetime(2023,4,1, tz='Asiz/Seoul'),
+    start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
     catchup=False
 ) as dag:
     '''서울시 코로나19 확진자 발생동향'''
